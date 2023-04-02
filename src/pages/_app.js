c3bfb0297739f3e96f032552a6910979c3d516ad
SeonjaeLee <tjswo9502@gmail.com>
2023-04-02 10:55:00 +0900

    :sparkles: Task-7: global스타일, 스타일변수, 폰트 설정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,18 @@
+import { Global } from '@emotion/react';
+import { Noto_Sans_KR } from 'next/font/google';
+
 import { KakaoMapProvider } from '@components';
+import { globalStyle } from '@styles';
+
+const notoSansKorea = Noto_Sans_KR({ weight: ['100', '400', '700'], subsets: ['latin'] });
 
 const App = ({ Component, pageProps }) => {
   return (
     <KakaoMapProvider>
-      <Component {...pageProps} />
+      <Global styles={globalStyle} />
+      <main className={notoSansKorea.className}>
+        <Component {...pageProps} />
+      </main>
     </KakaoMapProvider>
   );
 };
