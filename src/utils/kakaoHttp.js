35313908d2d5a072214e945f07ae281116b0f20e
SeonjaeLee <tjswo9502@gmail.com>
2023-04-28 00:24:05 +0900

    :sparkles: Task-10: 카카오 로컬 API 인스턴스 추가

---
+++ src/utils/kakaoHttp.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const KAKAO_LOCAL_URL = 'https://dapi.kakao.com/v2/local/';
+const GEO = 'geo';
+const SEARCH = 'search';
+
+const instance = axios.create({
+  baseURL: KAKAO_LOCAL_URL,
+  headers: {
+    Authorization: `KakaoAK ${process.env.REST_API}`,
+  },
+});
+
+instance.interceptors.request.use((config) => {
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const kakaoHttp = {
+  getConvertedAddress: async (longitude, latitude) => {
+    const { data } = await instance.get(`/${GEO}/coord2address`, { params: { x: longitude, y: latitude } });
+    return data;
+  },
+};
