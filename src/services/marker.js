a86d457ebd492b2d8fd94c0773e3794f6e679339
SeonjaeLee <tjswo9502@gmail.com>
2023-05-18 00:53:09 +0900

    :sparkles: Task-14: 마커리스트 불러오는 함수추가

--- src/services/marker.js
+++ src/services/marker.js
@@ -1,7 +1,12 @@
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 
 import { db } from '@utils';
 
 export const createMarker = async (marker) => {
   await addDoc(collection(db, 'marker'), marker);
+};
+
+export const getMarkerList = async () => {
+  const { docs } = await getDocs(collection(db, 'marker'));
+  return docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 };
