2a90d61a5d4967ce653a9535a2c2e59e7b7e0bc6
SeonjaeLee <tjswo9502@gmail.com>
2023-05-29 16:17:51 +0900

    :sparkles: Task-20: 회원가입페이지 이벤트 기능

---
+++ src/pages/signup/index.js
@@ -0,0 +1,53 @@
+import { useCallback, useMemo, useState } from 'react';
+import { useRouter } from 'next/router';
+
+import { Button } from '@components';
+
+const SignupPage = () => {
+  const { push } = useRouter();
+  const [form, setForm] = useState({ id: '', password: '', passwordCheck: '' });
+
+  const handleChange = useMemo(
+    () => ({
+      id: (e) => setForm((form) => ({ ...form, id: e.target.value })),
+      password: (e) => setForm((form) => ({ ...form, password: e.target.value })),
+      passwordCheck: (e) => setForm((form) => ({ ...form, passwordCheck: e.target.value })),
+    }),
+    []
+  );
+
+  const handleSubmit = useCallback(() => {
+    // TODO validation id, password, passwordCheck
+    // TODO SIGNUP API
+    console.log(form);
+  }, [form]);
+
+  const handleGoLoginPage = () => {
+    push('/login');
+  };
+
+  return (
+    <>
+      Signup
+      <input
+        placeholder="ID"
+        value={form.id}
+        onChange={handleChange.id}
+      />
+      <input
+        placeholder="Password"
+        value={form.password}
+        onChange={handleChange.password}
+      />
+      <input
+        placeholder="PasswordCheck"
+        value={form.passwordCheck}
+        onChange={handleChange.passwordCheck}
+      />
+      <Button onClick={handleSubmit}>회원가입</Button>
+      <Button onClick={handleGoLoginPage}>로그인페이지로 가기</Button>
+    </>
+  );
+};
+
+export default SignupPage;
