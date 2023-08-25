b610b331c94ac338d4bb1410ec78e16601bee50b
SeonjaeLee <tjswo9502@gmail.com>
2023-08-26 01:30:20 +0900

    :lipstick: Task-31: Layout 반응형 추가

--- src/components/_base/Layout/Layout.jsx
+++ src/components/_base/Layout/Layout.jsx
@@ -5,7 +5,11 @@ import { Noto_Sans_KR } from 'next/font/google';
 const notoSansKorea = Noto_Sans_KR({ weight: ['100', '400', '700'], subsets: ['latin'] });
 
 export const Layout = ({ children }) => {
-  return <Container className={notoSansKorea.className}>{children}</Container>;
+  return (
+    <Container className={notoSansKorea.className}>
+      <Wrapper>{children}</Wrapper>
+    </Container>
+  );
 };
 
 const Container = styled.main`
@@ -13,6 +17,22 @@ const Container = styled.main`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 100%;
   height: 100%;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  height: 100%;
+
+  @media screen and (min-width: 1024px) {
+    width: 600px;
+  }
+
+  @media screen and (max-width: 1023px) {
+    width: 390px;
+  }
+
+  @media screen and (max-width: 390px) {
+    width: 100%;
+  }
 `;
